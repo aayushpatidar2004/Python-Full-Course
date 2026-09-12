b860d0498129f0ec62d2eac805fbe03e3dbe6278
aayushpatidar2004 <patidaraayush20@gmail.com>
2026-09-12 18:28:03 +0530

    araa of circle

---
+++ operator/arithmaticOP/areaofcirle.py
@@ -0,0 +1,3 @@
+r=float(input("enter radius : "))
+ac=3.14*r**2
+print("ac of circle ={}".format(ac))
---
+++ operator/assignmentOP/ex2.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+a += b
+
+print("Result:", a)
