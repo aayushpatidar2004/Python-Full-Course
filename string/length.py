e6bab229df8736a43741b4b9e9653664b6f825cf
aayush patidar <patidaraayush20@gmail.com>
2026-09-25 18:48:23 +0530

    Delete string directory

--- string/length.py
+++
@@ -1,6 +0,0 @@
-# WAP to input user’s first name & print its length
-
-
-txt=input("enter the name : ")
-print("the name is = ")
-print("the length is = ",len(txt))
--- string/str.py
+++
@@ -1,4 +0,0 @@
-# WAP to find the occurrence of ‘$’ in a String.
-txt= input("enter the $ : ")
-count=txt.count("$")
-print(count)
